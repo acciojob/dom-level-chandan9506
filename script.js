ae999ea1cd3db5bc6ece7ea3b1bb3645b1dc4fe0
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-26 23:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const level = document.getElementById("level");
+
+
+const numberOfParents = getNumberOfParents(level);
+//alert("the level of the element is: ", numberOfParents);
+
+
+function getNumberOfParents(level) {
+    let count = 0;
+    let parent = level.parentElement; // Start with the direct parent of the element
+
+    // Traverse upwards until there are no more parent elements
+    while (parent !== null) {
+        count++; // Increment the count for each parent element found
+        parent = parent.parentElement; // Move to the next parent element
+    }
+
+    return count;
+	
+	
+}
